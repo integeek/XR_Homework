7b03a1cf649d684c86545801c7b300eb48c5eb47
integeek <ambre.giraudlanza@outlook.fr>
2024-02-23 13:36:16 +0200

    poubelle

--- XR_Homework_3/Assets/Scripts/Trigger.cs
+++ XR_Homework_3/Assets/Scripts/Trigger.cs
@@ -1,37 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
 
-public class Trigger : MonoBehaviour
+public class TrashCan : MonoBehaviour
 {
-    [SerializeField] UnityEvent onTriggerEnter;
-    [SerializeField] UnityEvent onTriggerExit;
+    public ParticleSystem particles; // Référence vers le système de particules
 
-    public GameObject particlesGameObject; // Référence vers le GameObject contenant le Particle System
-
-    void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
-        onTriggerEnter.Invoke();
-
-        // Activer les particules lorsque le déclencheur est activé
-        if (particlesGameObject != null)
+        // Vérifie si l'objet qui entre dans le trigger a un composant Rigidbody
+        Rigidbody rb = other.GetComponent<Rigidbody>();
+        if (rb != null)
         {
-            particlesGameObject.SetActive(true);
-        }
-
-        // Détruire l'objet détecté
-        Destroy(other.gameObject);
-    }
-
-    void OnTriggerExit(Collider other)
-    {
-        onTriggerExit.Invoke();
-
-        // Désactiver les particules lorsque le déclencheur est désactivé
-        if (particlesGameObject != null)
-        {
-            particlesGameObject.SetActive(false);
+            // Désactive le rendu de l'objet
+            other.gameObject.SetActive(false);
         }
     }
 }
