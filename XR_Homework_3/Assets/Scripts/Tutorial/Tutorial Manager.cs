84f14018142b5e57a93107b9c5250fa9be435ff9
integeek <ambre.giraudlanza@outlook.fr>
2024-03-13 10:23:41 +0200

    project finish

--- XR_Homework_3/Assets/Scripts/Tutorial/Tutorial Manager.cs
+++ XR_Homework_3/Assets/Scripts/Tutorial/Tutorial Manager.cs
@@ -16,8 +16,8 @@ public class TutorialManager : MonoBehaviour
     void Start()
     {
         tutorialMessages = new List<TutorialMessage>();
-        tutorialMessages.Add(new TutorialMessage("Welcome to your new kitchen.\nUse the left joystick to move yourself and the right joystick to move the camera.\nUse the trigger to grab and the Y button to reset the kitchen "));
-        tutorialMessages.Add(new TutorialMessage("You have to realize 3 orders : a sandwich, a pizza and a pie. \nThe recipes for each order are next to the material. \nWhen you have the dish, place it on the plate provided for this purpose and press the button to validate it."));
+        tutorialMessages.Add(new TutorialMessage("Welcome to your new kitchen.\nUse the left joystick to move yourself and the right joystick to move the camera.\nUse the trigger to grab"));
+        tutorialMessages.Add(new TutorialMessage("You have to realize 3 orders : a sandwich, a pizza and a pie. \nThe recipes for each order are next to the material. \nWhen you have the dish, place it on the plate provided for this purpose and press the button to validate it. \nFor the sandwich, just press the button next to the sandwich maker. "));
         tutorialMessages.Add(new TutorialMessage("You can cut with the knife, throw food in the trash and obviously open the fridge, the oven and the microwave. \nBread and plates are generated infinitely but this is not the case for food. \nGood luck in carrying out the 3 orders"));
         tutorialMessages.Add(new TutorialMessage("Congratulations ! You have successfully completed the game."));
 
